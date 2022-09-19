b2ac55cb5be7ee4a6157532fd1ca7763490c59fe
tanchik <negashovata@gmail.com>
2022-09-19 14:38:13 +0300

    добавлен файл для 5 задачи

--- zadachi.py
+++ zadachi.py
@@ -119,61 +119,102 @@
 # 3. Задача Создайте программу для игры в ""Крестики-нолики"".
 
 
-print('*'*100)
-print('\n')
-print('Давайте сыграем в крестики нолики!')
+# print('*'*100)
+# print('\n')
+# print('Давайте сыграем в крестики нолики!')
 
-board = list(range(1,10))
+# board = list(range(1,10))
 
-def design_board(board):
-    print('-'*12)
-    for i in range(3):
-        print('|', board[0+i*3],'|', board[1+i*3], '|', board[2+i*3], '|')
-        print('-'*12)
-def choice(tic_tac):
-    valid = False
-    while not valid:
-        player_index = input('Ваш ход, выберите ячейку ' + tic_tac + ' --> ')
-        try:
-            player_index =int(player_index)
-        except:
-            print('Что то не то нажали')
-            continue
-        if player_index >= 1 and player_index <= 9:
-            if(str(board[player_index-1]) not in 'XO'):
-                board[player_index-1] = tic_tac
-                valid = True
-            else:
-                print('Занято')
+# def design_board(board):
+#     print('-'*12)
+#     for i in range(3):
+#         print('|', board[0+i*3],'|', board[1+i*3], '|', board[2+i*3], '|')
+#         print('-'*12)
+# def choice(tic_tac):
+#     valid = False
+#     while not valid:
+#         player_index = input('Ваш ход, выберите ячейку ' + tic_tac + ' --> ')
+#         try:
+#             player_index =int(player_index)
+#         except:
+#             print('Что то не то нажали')
+#             continue
+#         if player_index >= 1 and player_index <= 9:
+#             if(str(board[player_index-1]) not in 'XO'):
+#                 board[player_index-1] = tic_tac
+#                 valid = True
+#             else:
+#                 print('Занято')
+#         else:
+#             print('Еще раз попробуй')
+
+# def victory_check(board):
+#     victory = ((0,1,2),(3,4,5),(6,7,8),
+#                (0,3,6),(1,4,7),(2,5,8),
+#                (0,4,8),(2,4,6))
+#     for i in victory:
+#         if board[i[0]] == board[i[1]] == board[i[2]]:
+#             return board[i[0]]
+#     return False
+
+# def game(board):
+#     counter =0
+#     vic = False
+#     while not vic:
+#         design_board(board)
+#         if counter % 2 == 0:
+#             choice('X')
+#         else:
+#             choice('0')
+#         counter +=1
+#         if counter > 4:
+#             tt_win = victory_check(board)
+#             if tt_win:
+#                 print(tt_win,'Победа')
+#                 vic = True
+#                 break
+#             if counter == 9:
+#                 print('Победила, ДРУЖБА)')
+#         design_board(board)
+# game(board)
+
+# Задача 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+
+with open('RLE1_decoded.txt', 'r') as data:
+    my_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
         else:
-            print('Еще раз попробуй')
+            count += 1
+    return str_code
 
-def victory_check(board):
-    victory = ((0,1,2),(3,4,5),(6,7,8),
-               (0,3,6),(1,4,7),(2,5,8),
-               (0,4,8),(2,4,6))
-    for i in victory:
-        if board[i[0]] == board[i[1]] == board[i[2]]:
-            return board[i[0]]
-    return False
+            
+str_code = encode_rle(my_text)
+print(str_code)
 
-def game(board):
-    counter =0
-    vic = False
-    while not vic:
-        design_board(board)
-        if counter % 2 == 0:
-            choice('X')
+with open('RLE2_encoded.txt', 'r') as data:
+    my_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
         else:
-            choice('0')
-        counter +=1
-        if counter > 4:
-            tt_win = victory_check(board)
-            if tt_win:
-                print(tt_win,'Победа')
-                vic = True
-                break
-            if counter == 9:
-                print('Победила, ДРУЖБА)')
-        design_board(board)
-game(board)
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(my_text2)
+print(str_decode)
