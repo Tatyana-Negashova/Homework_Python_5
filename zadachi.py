3c573fd1a330648cb48a74c353a6806eb931f3a0
tanchik <negashovata@gmail.com>
2022-09-19 14:28:53 +0300

    добавлена игра "крестики-нолики"

--- zadachi.py
+++ zadachi.py
@@ -72,45 +72,108 @@
 #     print(f"Выиграл {player2}")
 
 #a) Добавьте игру против бота. Достаточно сделать так, чтобы бот не брал конфет больше положенного или больше чем имеется в куче.
-from random import randint
+# from random import randint
 
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет: "))
-    return x
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
 
 
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
-player1 = input("Введите имя первого игрока: ")
-player2 = "Bot"
-value = int(input("Введите количество конфет на столе: "))
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
+# player1 = input("Введите имя первого игрока: ")
+# player2 = "Bot"
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
+# counter1 = 0 
+# counter2 = 0
 
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = randint(1,29)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = randint(1,29)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
 
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+
+
+# 3. Задача Создайте программу для игры в ""Крестики-нолики"".
+
+
+print('*'*100)
+print('\n')
+print('Давайте сыграем в крестики нолики!')
+
+board = list(range(1,10))
+
+def design_board(board):
+    print('-'*12)
+    for i in range(3):
+        print('|', board[0+i*3],'|', board[1+i*3], '|', board[2+i*3], '|')
+        print('-'*12)
+def choice(tic_tac):
+    valid = False
+    while not valid:
+        player_index = input('Ваш ход, выберите ячейку ' + tic_tac + ' --> ')
+        try:
+            player_index =int(player_index)
+        except:
+            print('Что то не то нажали')
+            continue
+        if player_index >= 1 and player_index <= 9:
+            if(str(board[player_index-1]) not in 'XO'):
+                board[player_index-1] = tic_tac
+                valid = True
+            else:
+                print('Занято')
+        else:
+            print('Еще раз попробуй')
+
+def victory_check(board):
+    victory = ((0,1,2),(3,4,5),(6,7,8),
+               (0,3,6),(1,4,7),(2,5,8),
+               (0,4,8),(2,4,6))
+    for i in victory:
+        if board[i[0]] == board[i[1]] == board[i[2]]:
+            return board[i[0]]
+    return False
+
+def game(board):
+    counter =0
+    vic = False
+    while not vic:
+        design_board(board)
+        if counter % 2 == 0:
+            choice('X')
+        else:
+            choice('0')
+        counter +=1
+        if counter > 4:
+            tt_win = victory_check(board)
+            if tt_win:
+                print(tt_win,'Победа')
+                vic = True
+                break
+            if counter == 9:
+                print('Победила, ДРУЖБА)')
+        design_board(board)
+game(board)
